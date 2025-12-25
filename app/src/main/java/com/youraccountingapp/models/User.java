a3f35c4d0c6675c04fa67d668d2dd50d90fcf6c7
geromansur0-a11y <geromansur0@gmail.com>
2025-12-25 12:34:17 +0700

    Create User.java

---
+++ app/src/main/java/com/youraccountingapp/models/User.java
@@ -0,0 +1,13 @@
+package com.youraccountingapp.models;
+
+public class User {
+    private int id;
+    private String username;
+    private String email;
+    private String passwordHash;
+    private String companyName;
+    private Date createdAt;
+    
+    // Constructor, Getters, Setters
+    // ...
+}
